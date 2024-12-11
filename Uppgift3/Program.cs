239f7d65460f208c2c9a8366f8f813ef503b23b3
arsu07001 <arsu07001@E-PW05CGQW>
2024-12-11 13:35:16 +0100

    Add project files.

---
+++ Uppgift3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int summa = 0;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Skriv in heltal nummer {i}: ");
+            int tal = int.Parse(Console.ReadLine());
+            summa += tal;
+        }
+
+        Console.WriteLine($"Summan av de inmatade talen är: {summa}");
+    }
+}
